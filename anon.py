69978710a664b10d367e66a01286872f962b888e
markitics <m@rkmoriarty.com>
2024-11-06 03:33:36 -0800

    Move anon secr deets from code to .env (used in Bearer header; also email is POSTed).

--- anon.py
+++ anon.py
@@ -1,17 +1,21 @@
 import requests
 import logging
+import os
+from dotenv import load_dotenv
+import time
 
+load_dotenv()  # Add this to load .env file
 
 def get_instacart_url_from_anon() -> tuple[str, str]:
     url = "https://svc.sandbox.anon.com/account/api/v1/cdpUrl"
 
     payload = {
         "apps": ["instacart"],
-        "appUserId": "sansburyjacob@gmail.com",
+        "appUserId": os.getenv("ANON_APP_USER_ID"),
         "proxy": False,
     }
     headers = {
-        "Authorization": "Bearer anon_Psst/WzqSJTg0gat06QlSgOya+QFfdEST+xa9sXcnIaXK9OrB9ohiTm0H5FmAFYGqfrBOnrrtrRrEHQj",
+        "Authorization": f"Bearer {os.getenv('ANON_API_KEY')}",
         "Content-Type": "application/json",
     }
 
@@ -20,6 +24,8 @@ def get_instacart_url_from_anon() -> tuple[str, str]:
     url = response.json().get("cdpUrl")
 
     print("Debug url: ", response.json().get("liveStreamingUrl"))
+    print("Waiting 7 seconds for you to open the url in your browser, in case you'd like to follow along")
+    time.sleep(7)
 
     if not url:
         logging.error("No cdpUrl found in response")
